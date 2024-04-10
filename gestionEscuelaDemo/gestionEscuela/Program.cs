f464dd589501f468e18d79bc527ed192c580b843
AlvinRHD <ezequielhernandes907@gmail.com>
2024-04-10 14:50:52 -0600

    ProjectStart

---
+++ gestionEscuelaDemo/gestionEscuela/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace gestionEscuela
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
